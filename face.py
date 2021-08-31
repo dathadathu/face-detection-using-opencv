a2021db6a4ca4297c7b67c0208ae091d8e72a4d2
SHIVA DATHA <58063053+dathadathu@users.noreply.github.com>
2021-08-31 20:55:06 +0530

    Error resolved

--- face.py
+++ face.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2
+
 faceCascade = cv2.CascadeClassifier('D:\Projects\face detection\haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
+
 while True:
     ret, img = cap.read()
     img = cv2.flip(img, 1)
@@ -22,5 +24,7 @@ while True:
     k = cv2.waitKey(30) & 0xff
     if k == 27: # press 'ESC' to quit
         break
+
 cap.release()
+
 cv2.destroyAllWindows()
